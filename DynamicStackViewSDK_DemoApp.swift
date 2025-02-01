d972f2f6861c47f27c2fb02d86f105c6a3e604a4
filipinius <filipinius@gmail.com>
2025-02-01 21:53:40 +0100

    Initial commit with SDK submodule

---
+++ DynamicStackViewSDK_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicStackViewSDK_DemoApp.swift
+//  DynamicStackViewSDK-Demo
+//
+//  Created by Filip Vukadinovic on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DynamicStackViewSDK_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
